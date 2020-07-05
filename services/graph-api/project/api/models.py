6c09150197f67b74580f7414d4190b095050f4b6
Diego Quintana <daquintanav@gmail.com>
2020-07-06 01:18:20 +0200

    new: dev: add basic Victim model and schema, queryable from graphql
    not tested, this needs tests!

--- services/graph-api/project/api/models.py
+++ services/graph-api/project/api/models.py
@@ -2,7 +2,7 @@ import os
 import maya
 from graphql import GraphQLError
 from py2neo import Graph
-from py2neo.ogm import GraphObject, Property, RelatedTo
+from py2neo.ogm import GraphObject, Property, Label, RelatedTo
 
 from flask import current_app
 
@@ -35,161 +35,41 @@ class BaseModel(GraphObject):
         graph.push(self)
 
 
-class Product(BaseModel):
-    __primarykey__ = 'name'
+class Victim(BaseModel):
+    """Base class for a person"""
+    __primarykey__ = 'individual_id'
 
-    name = Property()
-    brand = Property()
-    category = Property()
+    # from data
+    individual_id = Property()
+    group_id = Property()
+    first_name = Property()  # todo: use Label for matching, see https://py2neo.org/v4/ogm.html#labels
+    last_name = Property()  # todo: use Label for matching, see https://py2neo.org/v4/ogm.html#labels
+    age = Property()
+    minor = Property()
+    male = Property()
+    number_previous_arrests = Property()
+    occupation = Property()
+    occupation_detail = Property()
+    victim_affiliation = Property()
+    victim_affiliation_detail = Property()
+    targeted = Property()
 
     def as_dict(self):
         return {
-            'name': self.name,
-            'brand': self.brand,
-            'category': self.category
+            'individual_id': self.individual_id,
+            'group_id': self.group_id,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age,
+            'minor': self.minor,
+            'male': self.male,
+            'number_previous_arrests': self.number_previous_arrests,
+            'occupation': self.occupation,
+            'occupation_detail': self.occupation_detail,
+            'victim_affiliation': self.victim_affiliation,
+            'victim_affiliation_detail': self.victim_affiliation_detail,
+            'targeted': self.targeted,
         }
 
     def fetch(self):
-        return self.match(graph, self.name).first()
-
-
-class Store(BaseModel):
-    name = Property()
-    address = Property()
-
-    products = RelatedTo('Product', 'SELLS')
-    receipts = RelatedTo('Product', 'EMITTED')
-
-    def fetch(self, _id):
-        return Store.match(graph, _id).first()
-
-    def fetch_by_name_and_address(self):
-        return Store.match(graph).where(
-            f'_.name = "{self.name}" AND _.address = "{self.address}"'
-        ).first()
-
-    def fetch_products(self):
-        return [{
-            **product[0].as_dict(),
-            **product[1]
-        } for product in self.products._related_objects]
-
-    def as_dict(self):
-        return {
-            '_id': self.__primaryvalue__,
-            'name': self.name,
-            'address': self.address
-        }
-
-
-class Receipt(BaseModel):
-    total_amount = Property()
-    timestamp = Property()
-
-    products = RelatedTo('Product', 'HAS')
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-        if kwargs.get('validate', False):
-            self.__validate_timestamp()
-
-    def as_dict(self):
-        return {
-            '_id': self.__primaryvalue__,
-            'total_amount': self.total_amount,
-            'timestamp': maya.parse(self.timestamp)
-        }
-
-    def fetch(self, _id):
-        return self.match(graph, _id).first()
-
-    def fetch_products(self):
-        return [{
-            **product[0].as_dict(),
-            **product[1]
-        } for product in self.products._related_objects]
-
-    def __validate_timestamp(self):
-        try:
-            maya.parse(self.timestamp, day_first=True, year_first=False)
-        except Exception:
-            raise GraphQLError(
-                'The timestamp you provided is not within the format: "dd/mm/yyyy hh:mm"'
-            )
-
-
-class Customer(BaseModel):
-    __primarykey__ = 'email'
-    name = Property()
-    email = Property()
-
-    receipts = RelatedTo('Receipt', 'HAS')
-    stores = RelatedTo('Store', 'GOES_TO')
-
-    def fetch(self):
-        customer = self.match(graph, self.email).first()
-        if customer is None:
-            raise GraphQLError(
-                f'"{self.email}" has not been found in our customers list.')
-
-        return customer
-
-    def as_dict(self):
-        return {
-            'email': self.email,
-            'name': self.name
-        }
-
-    def __verify_products(self, products):
-        _total_amount = 0
-        for product in products:
-            _product = Product(name=product.get('name')).fetch()
-            if _product is None:
-                raise GraphQLError(
-                    f'"{product.name}" has not been found in our products list.')
-
-            _total_amount += product['price'] * product['amount']
-            product['product'] = _product
-        return products, _total_amount
-
-    def __verify_receipt(self, receipt):
-        customer_properties = f":Customer {{email: '{self.email}'}}"
-        receipt_properties = f":Receipt {{timestamp: '{receipt.timestamp}', total_amount:{receipt.total_amount}}}"
-        existing_receipts = graph.run(
-            f"MATCH ({customer_properties})-[relation:HAS]-({receipt_properties}) RETURN relation").data()
-
-        if existing_receipts:
-            raise GraphQLError(
-                "The receipt you're trying to submit already exists.")
-
-    def __link_products(self, products, total_amount, timestamp):
-        receipt = Receipt(total_amount=total_amount,
-                          timestamp=timestamp, validate=True)
-        self.__verify_receipt(receipt)
-
-        for item in products:
-            receipt.products.add(item.pop('product'), properties=item)
-
-        return receipt
-
-    def __verify_store(self, store):
-        _store = Store(**store).fetch_by_name_and_address()
-        if _store is None:
-            raise GraphQLError(
-                f"The store \"{store['name']}\" does not exist in our stores list.")
-
-        return _store
-
-    def __add_links(self, store, receipt):
-        store.receipts.add(receipt)
-        self.stores.add(store)
-        self.receipts.add(receipt)
-
-    def submit_receipt(self, products, timestamp, store):
-        self.__add_links(
-            self.__verify_store(store),
-            self.__link_products(*self.__verify_products(products), timestamp)
-        )
-
-        self.save()
+        return self.match(graph, self.individual_id).first()
--- services/graph-api/project/api/schemas.py
+++ services/graph-api/project/api/schemas.py
@@ -1,128 +1,38 @@
 import graphene
 
-from .models import Customer, Store, Receipt, Product
+from .models import Victim
 
 
-class ProductSchema(graphene.ObjectType):
-    name = graphene.String()
-    brand = graphene.String()
-    category = graphene.String()
-
-    price = graphene.Float()
-    amount = graphene.Int()
-
-
-class ProductInput(graphene.InputObjectType):
-    name = graphene.String(required=True)
-    price = graphene.Float(required=True)
-    amount = graphene.Int(required=True)
-
-
-class StoreSchema(graphene.ObjectType):
-    name = graphene.String()
-    address = graphene.String()
-
-    products = graphene.List(ProductSchema)
-
-    def __init__(self, **kwargs):
-        self._id = kwargs.pop('_id')
-        super().__init__(**kwargs)
-
-    def resolve_products(self, info):
-        return [ProductSchema(**product) for product in Store().fetch(self._id).fetch_products()]
-
-
-class StoreInput(graphene.InputObjectType):
-    name = graphene.String(required=True)
-    address = graphene.String(required=True)
-
-
-class ReceiptSchema(graphene.ObjectType):
-    total_amount = graphene.Float()
-    timestamp = graphene.String()
-
-    products = graphene.List(ProductSchema)
-
-    def __init__(self, **kwargs):
-        self._id = kwargs.pop('_id')
-        super().__init__(**kwargs)
-
-    def resolve_products(self, info):
-        return [ProductSchema(**product) for product in Receipt().fetch(self._id).fetch_products()]
-
-
-class SubmitReceipt(graphene.Mutation):
-    class Arguments:
-        customer_email = graphene.String(required=True)
-        products = graphene.List(graphene.NonNull(ProductInput))
-        store = StoreInput(required=True)
-        timestamp = graphene.String(required=True)
-
-    success = graphene.Boolean()
-
-    def mutate(self, info, **kwargs):
-        customer = Customer(email=kwargs.pop('customer_email')).fetch()
-        customer.submit_receipt(**kwargs)
-
-        return SubmitReceipt(success=True)
-
-
-class CustomerSchema(graphene.ObjectType):
-    email = graphene.String()
-    name = graphene.String()
-
-    stores = graphene.List(StoreSchema)
-    receipts = graphene.List(ReceiptSchema)
-    products = graphene.List(ProductSchema)
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.customer = Customer(email=self.email).fetch()
-
-    def resolve_stores(self, info):
-        return [StoreSchema(**store.as_dict()) for store in self.customer.stores]
-
-    def resolve_receipts(self, info):
-        return [ReceiptSchema(**receipt.as_dict()) for receipt in self.customer.receipts]
-
-    def resolve_products(self, info):
-        return [ProductSchema(**product.as_dict()) for product in self.customer.products]
-
-
-class CreateCustomer(graphene.Mutation):
-    class Arguments:
-        name = graphene.String(required=True)
-        email = graphene.String(required=True)
-
-    success = graphene.Boolean()
-    customer = graphene.Field(lambda: CustomerSchema)
-
-    def mutate(self, info, **kwargs):
-        customer = Customer(**kwargs)
-        customer.save()
-
-        return CreateCustomer(customer=customer, success=True)
+class VictimSchema(graphene.ObjectType):
+    individual_id = graphene.Int()
+    group_id = graphene.Int()
+    first_name = graphene.String()
+    last_name = graphene.String()
+    age = graphene.String()  # support for "NA"
+    minor = graphene.Boolean()
+    male = graphene.Boolean()
+    number_previous_arrests = graphene.String()
+    occupation = graphene.String()
+    occupation_detail = graphene.String()
+    victim_affiliation = graphene.String()
+    victim_affiliation_detail = graphene.String()
+    targeted = graphene.String()
 
 
 class Query(graphene.ObjectType):
-    customer = graphene.Field(lambda: CustomerSchema, email=graphene.String())
-    stores = graphene.List(lambda: StoreSchema)
-    products = graphene.List(lambda: ProductSchema)
 
-    def resolve_customer(self, info, email):
-        customer = Customer(email=email).fetch()
-        return CustomerSchema(**customer.as_dict())
+    # this defines a Field `hello` in our Schema with a single Argument `name`
 
-    def resolve_stores(self, info):
-        return [StoreSchema(**store.as_dict()) for store in Store().all]
+    victims = graphene.List(lambda: VictimSchema)
+    victim = graphene.Field(lambda: VictimSchema,
+                            individual_id=graphene.Int())
 
-    def resolve_products(self, info):
-        return [ProductSchema(**product.as_dict()) for product in Product().all]
+    def resolve_victims(root, info):
+        return [VictimSchema(**victim.as_dict()) for victim in Victim().all]
 
-
-class Mutations(graphene.ObjectType):
-    create_customer = CreateCustomer.Field()
-    submit_receipt = SubmitReceipt.Field()
+    def resolve_victim(root, info, individual_id):
+        victim = Victim(individual_id=individual_id).fetch()
+        return VictimSchema(**victim.as_dict())
 
 
-schema = graphene.Schema(query=Query, mutation=Mutations, auto_camelcase=False)
+schema = graphene.Schema(query=Query, auto_camelcase=False)
