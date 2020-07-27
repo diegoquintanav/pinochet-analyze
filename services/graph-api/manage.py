ac1147b58e7bfb5b79ce0be2a69c853093aca4dc
Diego Quintana <daquintanav@gmail.com>
2020-07-27 10:39:39 +0200

    fix poor implementation of app at manage.py
    see https://github.com/pallets/flask/issues/3701 for details

--- services/graph-api/manage.py
+++ services/graph-api/manage.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 import click
-from flask import app
+from flask import current_app
 from flask.cli import FlaskGroup
 
 from project import create_app
@@ -9,19 +9,16 @@ from project.db import clear_graph, seed_graph
 
 # create_app is not handled correctly
 # see https://github.com/pallets/flask/issues/3701
-app = create_app()
-
-cli = FlaskGroup(create_app=create_app)
-
+cli = FlaskGroup(create_app=create_app, set_debug_flag=False)
 
 @cli.command("delete_db")
 @click.option("--dry-run", is_flag=True)
 def delete_db(dry_run):
-    app.logger.info("Deleting graph")
+    current_app.logger.info("Deleting graph")
     status = clear_graph(dry_run=dry_run)
     for k, v in status.items():
-        app.logger.info(k + ": " + str(v))
-    app.logger.info("Done")
+        current_app.logger.info(k + ": " + str(v))
+    current_app.logger.info("Done")
 
 
 @cli.command("seed_db")
@@ -29,29 +26,29 @@ def delete_db(dry_run):
 def seed_db(dry_run):
     PINOCHET_CSV = os.environ.get(
         "PINOCHET_CSV", Path(__file__).parent.joinpath("project", "db", "pinochet.csv"))
-    app.logger.info("Seeding tables")
+    current_app.logger.info("Seeding tables")
     status = seed_graph(filepath=PINOCHET_CSV, dry_run=dry_run)
-    app.logger.info(status)
-    app.logger.info("Done")
+    current_app.logger.info(status)
+    current_app.logger.info("Done")
 
 
 @cli.command("recreate_db")
 @click.option("--dry-run", is_flag=True)
 @click.pass_context
 def recreate_db(ctx, dry_run):
-    app.logger.info("Deleting graph")
+    current_app.logger.info("Deleting graph")
     ctx.invoke(delete_db, dry_run=dry_run)
-    app.logger.info("Recreating graph")
+    current_app.logger.info("Recreating graph")
     ctx.invoke(seed_db, dry_run=dry_run)
-    app.logger.info("Done")
+    current_app.logger.info("Done")
 
 
 @cli.command("app_config")
 def app_config():
-    print("app.debug from cli.command")
-    print(app.debug)
-    print("app.config from cli.command")
-    print(app.config)
+    print("current_app.debug from cli.command")
+    print(current_app.debug)
+    print("current_app.config from cli.command")
+    print(current_app.config)
 
 
 if __name__ == "__main__":
